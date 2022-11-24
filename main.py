eef1cc9921c82f20dee8f0b3bd19d7a0603b144f
cdchavezq@unal.edu.co <cdchavezq@unal.edu.co>
2022-11-24 07:05:24 -0300

    finished the implementation

--- main.py
+++ main.py
@@ -2,17 +2,39 @@ from models.bipartite_graph import BipartiteGraph
 
 
 def main():
-
-    #This is a graph where all vertices on one side are connected to a single vertex on the other. 
-    #Therefore the maximum should be 1
     
-    bpGraph =[[1,0,0],
-          [1, 0, 0],
-          [1, 0, 0]]
+    bpGraph =[
+        [1,0,0],
+        [0,1,0],
+        [0,0,1]
+          ]
 
-    g = BipartiteGraph(bpGraph)
+    trivialCase = BipartiteGraph(bpGraph)
 
-    print(f"Maximum number of bipartite matchings would be {g.maximumBipartiteMatching()}")
+    print(f"At the trivial case there should be a maximum of 3 matchings which is {trivialCase.getMaximumNumberOfMatches()==3}")
+
+
+    bpGraph =[
+        [1,0,0],
+        [1,0,0],
+        [1,0,0]
+          ]
+
+    trivialCase = BipartiteGraph(bpGraph)
+
+    print(f"Now every vertex from the source is connected to a single vertex therefore there should only 1 matching which is {trivialCase.getMaximumNumberOfMatches()==1}")
+
+
+
+    bpGraph =[
+        [0,1,0],
+        [1,0,0],
+        [1,0,0]
+          ]
+
+    trivialCase = BipartiteGraph(bpGraph)
+
+    print(f"For this last case by looking at the given matrix we should have 2 matches, which is {trivialCase.getMaximumNumberOfMatches()==2}")
 
 
 
--- models/bipartite_graph.py
+++ models/bipartite_graph.py
@@ -1,41 +1,41 @@
+from typing import List
+
 class BipartiteGraph:
-    def __init__(self,graph):
-         
-        
+
+    numberOfSourceVertices:int = 0
+    numberOfTargetVertices:int = 0
+    matches:List[int] = None
+
+    def __init__(self,graph:List[List]):
         self.graph = graph
-        self.numberOfSourceVertices = len(graph)
-        self.numberOfTargetVertices = len(graph[0])
- 
+        if len(graph) > 0:
+            self.numberOfSourceVertices = len(graph)
+            self.numberOfTargetVertices = len(graph[0])
+            self.matches = [-1 for _ in range(self.numberOfTargetVertices)] #This vector represents the matched correspondances. 
+        else:
+            raise ValueError("The matrix should have at least one element")
     
-    def matchVertex(self, u, matchedVerticesTarget, seen):
- 
-        
-        for v in range(self.numberOfTargetVertices):
- 
-            
-            
-            if self.graph[u][v]==1 and seen[v] == False:
-                 
-                
-                seen[v] = True
- 
-                
-                if matchedVerticesTarget[v] == -1 or self.matchVertex(matchedVerticesTarget[v],matchedVerticesTarget, seen):
-                    matchedVerticesTarget[v] = u
+
+
+
+
+
+    def traversePossibleMatches(self,sourceVertex:int,visited:List[bool])->bool:
+        for targetVertice in range(self.numberOfTargetVertices):
+            if self.graph[sourceVertex][targetVertice] == 1 and visited[targetVertice]==False:
+                visited[targetVertice] = True
+
+                if self.matches[targetVertice] == -1 or self.traversePossibleMatches(self.matches[targetVertice],visited):
+                    self.matches[targetVertice] = sourceVertex
                     return True
         return False
- 
-    
-    def maximumBipartiteMatching(self):
+
+    def getMaximumNumberOfMatches(self):
         
-        matchedVerticesTarget = [-1  for i in  range(self.numberOfTargetVertices)]
-         
-        
-        finalResult = 0
+        maxNumberMatches:int = 0
         for i in range(self.numberOfSourceVertices):
-     
-            
-            seen = [False for i in range(self.numberOfTargetVertices)]
-            if self.matchVertex(i, matchedVerticesTarget, seen):
-                finalResult += 1
-        return finalResult
+            visited:List[bool] = [False for _ in range(self.numberOfTargetVertices)]
+            if self.traversePossibleMatches(sourceVertex=i,visited=visited):
+                maxNumberMatches+=1
+        
+        return maxNumberMatches
